dc6907f20d63ed4a91f81c3c17573ed4801ac4ab
Alex Gorodov <a.gorodov.22@gmail.com>
2023-08-27 05:26:49 +0300

    add link to main page on 404

--- src/pages/page-not-exist/page-not-exist.tsx
+++ src/pages/page-not-exist/page-not-exist.tsx
@@ -1,7 +1,10 @@
+import { Link } from "react-router-dom";
+
 export function PageNotExist(): JSX.Element {
   return (
     <div className="not-exist">
       <p className="not-exist__title">404</p>
+      <Link to="saas/dashboard">to main page</Link>
     </div>
   );
 }
